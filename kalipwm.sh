4d5c8d8dce0cb3bb2db53a7a925f448e4aab6479
afsh4ck <132138425+afsh4ck@users.noreply.github.com>
2024-07-03 15:59:46 +0200

    Update kalipwm.sh

--- kalipwm.sh
+++ kalipwm.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# Check if current user is root
+# Comprobar si el usuario actual es root
 if [ "$UID" -eq 0 ]; then
-    echo "Cannot run as root."
+    echo "No se puede ejecutar como root."
     exit 1
 else
-    # Checks for sudo
+    # Comprobar si se está usando sudo
     if [ -n "$SUDO_USER" ]; then
-        echo "Do not use sudo"
+        echo "No uses sudo"
         exit 1
     fi
 fi
@@ -21,60 +21,60 @@ echo "
 ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚═╝╚═╝      ╚══╝╚══╝ ╚═╝     ╚═╝
 "
 sleep 2
-echo -e "[+] Professional Hacking environment automation script."
-echo -e "[+] @afsh4ck - Follow me on YouTube & Instagram!"
+echo -e "[+] Script de automatización de entorno de hacking profesional."
+echo -e "[+] @afsh4ck - ¡Sígueme en YouTube e Instagram!"
 sleep 3
-echo -e "\n[*] Setting up the installation..\n"
+echo -e "\n[*] Configurando la instalación..\n"
 sleep 4
 
 RPATH=`pwd`
 
-# update and upgrade all
- sudo apt update && sudo apt -y full-upgrade
+# Actualizar y actualizar todo
+sudo apt update && sudo apt -y full-upgrade
 
-# install packages 
+# Instalar paquetes
 sudo apt install -y git vim feh scrot scrub zsh rofi xclip xsel locate neofetch wmname acpi bspwm sxhkd \
 imagemagick ranger kitty tmux python3-pip font-manager lsd bpython open-vm-tools-desktop open-vm-tools # snapd
 
-# install environment dependencies
+# Instalar dependencias del entorno
 sudo apt install -y build-essential libxcb-util0-dev libxcb-ewmh-dev libxcb-randr0-dev \
 libxcb-icccm4-dev libxcb-keysyms1-dev libxcb-xinerama0-dev libasound2-dev libxcb-xtest0-dev libxcb-shape0-dev # (xcb removed)
 
-# install polybar requirements
+# Instalar requisitos de polybar
 sudo apt install -y cmake cmake-data pkg-config python3-sphinx libcairo2-dev libxcb1-dev libxcb-util0-dev \
 libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev \
 libxcb-icccm4-dev libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev libjsoncpp-dev \
 libmpdclient-dev libuv1-dev libnl-genl-3-dev
 
-# Install picom dependencies
+# Instalar dependencias de picom
 sudo apt install -y meson libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev \
 libxcb-render-util0-dev libxcb-render0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev \
 libxcb-xinerama0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev libpcre2-dev libevdev-dev \
 uthash-dev libev-dev libx11-xcb-dev libxcb-glx0-dev libpcre3 libpcre3-dev
 
-# install fonts
+# Instalar fuentes
 mkdir /tmp/fonts
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/Hack.zip -O /tmp/fonts/Hack.zip
 unzip /tmp/fonts/Hack.zip -d /tmp/fonts
 font-manager -i /tmp/fonts/*.ttf
 
-# install ohmyzsh
+# Instalar ohmyzsh
 rm -rf ~/.oh-my-zsh
 yes | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-# install powerlevel10k
+# Instalar powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 rm -f ~/.p10k.zsh
 cp -v $RPATH/CONFIGS/p10k.zsh ~/.p10k.zsh
 
-# install zsh plugins
+# Instalar plugins de zsh
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 rm -f ~/.zshrc
-# install zsh-autocomplete?
+# ¿Instalar zsh-autocomplete?
 cp -v $RPATH/CONFIGS/zshrc ~/.zshrc
 
-# install fzf
+# Instalar fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 yes | ~/.fzf/install
 
@@ -90,10 +90,10 @@ sudo tar xzvf /tmp/nvim-linux64.tar.gz --directory=/opt
 sudo ln -s /opt/nvim-linux64/bin/nvim /usr/bin/nvim
 sudo rm -f /opt/nvim-linux64.tar.gz
 
-#nvchad - needs work. Block cursor and user interaction
+# nvchad - necesita trabajo. Bloquear cursor e interacción del usuario
 # git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && nvim
 
-# install kitty terminal
+# Instalar terminal kitty
 cat $RPATH/kitty-installer.sh | sh /dev/stdin
 # ~/.local/kitty.app/bin/kitty
 
@@ -101,12 +101,12 @@ cat $RPATH/kitty-installer.sh | sh /dev/stdin
 wget https://github.com/sharkdp/bat/releases/download/v0.24.0/bat_0.24.0_amd64.deb -O /tmp/bat.deb
 sudo dpkg -i /tmp/bat.deb
 
-# Clone polybar & picom repos
+# Clonar repositorios de polybar & picom
 mkdir ~/github
 git clone --recursive https://github.com/polybar/polybar ~/github/polybar
 git clone https://github.com/ibhagwan/picom.git ~/github/picom
 
-# install polybar
+# Instalar polybar
 cd ~/github/polybar
 mkdir build
 cd build
@@ -114,62 +114,62 @@ cmake ..
 make -j$(nproc)
 sudo make install
 
-# install polybar themes
+# Instalar temas de polybar
 git clone --depth=1 https://github.com/adi1090x/polybar-themes.git ~/github/polybar-themes
 chmod +x ~/github/polybar-themes/setup.sh
 cd ~/github/polybar-themes
 echo 1 | ./setup.sh
 
-# install picom
+# Instalar picom
 cd ~/github/picom
 git submodule update --init --recursive
 meson --buildtype=release . build
 ninja -C build
 sudo ninja -C build install
 
-# Change timezone
-# To list timezones run: timedatectl list-timezones
+# Cambiar zona horaria
+# Para listar zonas horarias ejecutar: timedatectl list-timezones
 sudo timedatectl set-timezone "Europe/Madrid"
 
 mkdir ~/screenshots
-# Copy all config files
+# Copiar todos los archivos de configuración
 cp -rv $RPATH/CONFIGS/config/* ~/.config/
 
-# Copy scripts
+# Copiar scripts
 cp -rv $RPATH/SCRIPTS/* ~/.config/polybar/forest/scripts/
 sudo ln -s ~/.config/polybar/forest/scripts/target.sh /usr/bin/target
 sudo ln -s ~/.config/polybar/forest/scripts/screenshot.sh /usr/bin/screenshot
 
-# Copy wallpapers
+# Copiar wallpapers
 mkdir ~/Wallpapers/
 cp -rv $RPATH/WALLPAPERS/* ~/Wallpapers/
 
-# Set execution perms
+# Establecer permisos de ejecución
 chmod +x ~/.config/bspwm/bspwmrc
 chmod +x ~/.config/bspwm/scripts/bspwm_resize
 chmod +x ~/.config/polybar/launch.sh
 chmod +x ~/.config/polybar/forest/scripts/target.sh
 chmod +x ~/.config/polybar/forest/scripts/screenshot.sh
 
-# Select rofi theme
-#rofi-theme-selector
+# Seleccionar tema de rofi
+# rofi-theme-selector
 
-# Enable tap to click and change mousepad scroll direction (laptops) https://cravencode.com/post/essentials/enable-tap-to-click-in-i3wm/
-#sudo mkdir -p /etc/X11/xorg.conf.d && sudo tee <<'EOF' /etc/X11/xorg.conf.d/90-touchpad.conf 1> /dev/null
-#Section "InputClass"
+# Habilitar toque para hacer clic y cambiar dirección de desplazamiento del touchpad (portátiles) https://cravencode.com/post/essentials/enable-tap-to-click-in-i3wm/
+# sudo mkdir -p /etc/X11/xorg.conf.d && sudo tee <<'EOF' /etc/X11/xorg.conf.d/90-touchpad.conf 1> /dev/null
+# Section "InputClass"
 #        Identifier "touchpad"
 #        MatchIsTouchpad "on"
 #        Driver "libinput"
 #        Option "Tapping" "on"
 #        Option "NaturalScrolling" "on"
-#EndSection
+# EndSection
 #
-#EOF
+# EOF
 
-# Clean files
+# Limpiar archivos
 rm -rf ~/github
 rm -rf $RPATH
 sudo apt autoremove -y
 
-echo -e "\n[+] Done. Happy Hacking ;)\n"
-echo -e "[!] Please reboot..\n"
+echo -e "\n[+] Listo, Happy Hacking ;)"\n
+echo -e "\n[+] Por favor, reinicia el equipo;)"\n
