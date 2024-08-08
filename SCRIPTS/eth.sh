cb3d36ea16f8d390c865f97057a52fcbd6b9c2f3
PoleG97 <44400218+PoleG97@users.noreply.github.com>
2024-08-08 20:09:26 +0200

    añadidos archivos de visualización y modificación de config de la poly

---
+++ SCRIPTS/eth.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Obtener la IP de la VPN. Se asume que la interfaz de la VPN es tun0.
+vpn_ip=$(ip addr show eth0 2>/dev/null | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
+
+if [ $? -ne 0 ]; then
+    # Capturar el error del comando 'ip addr show'
+    printf "No VPN"
+else
+    # Se encontró la IP
+    echo "$vpn_ip"
+fi
