73aa6d30a5127c991c1a37ed45c098daebb56f22
PoleG97 <44400218+PoleG97@users.noreply.github.com>
2024-08-09 20:20:31 +0200

    cambio de nombres

--- SCRIPTS/wifi.sh
+++ SCRIPTS/wifi.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Obtener la IP de la VPN. Se asume que la interfaz de la VPN es tun0.
-vpn_ip=$(ip addr show wlan0 2>/dev/null | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
+wifi_ip=$(ip addr show wlan0 2>/dev/null | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
 
 if [ $? -ne 0 ]; then
     # Capturar el error del comando 'ip addr show'
-    printf "No VPN"
+    printf "No WiFi"
 else
     # Se encontró la IP
-    echo "$vpn_ip"
+    echo "$wifi_ip"
 fi
